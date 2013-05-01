d9f5734ed5090236693b33167e6e35bdf4402d40
Athanasios <lixtetrax@grhack.net>
2013-05-01 12:21:23 +0200

    optimized

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 
 for i in xrange(1,101):
-	if i % 5 == 0 and i % 3 == 0:
-		print "FizzBuzz"
-	elif i % 3 == 0:
-		print "Fizz"
-	elif i % 5 == 0:
-		print "Buzz"
-	else:
-		print i
+	msg = ""
+	if i % 3 == 0:
+		msg += "Fizz"
+	if i % 5 == 0:
+		msg += "Buzz"
+	print msg or i 
 
