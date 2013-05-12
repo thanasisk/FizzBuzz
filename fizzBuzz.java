d8cf796438be01543bf503b91ce7fa6050add515
Athanasios <lixtetrax@grhack.net>
2013-05-12 16:55:13 +0200

    added fizzBuzz Java version

--- fizzBuzz.java
+++ fizzBuzz.java
@@ -1,11 +1,11 @@
-public class fizzBuzz {                      // Everything in Java is a class
-  public static void main(String[] args) {   // Every program must have main()
-    for(int i = 1; i <= 100; i++) {                    // count from 1 to 100
-      if (((i % 3) == 0) && ((i % 5) == 0))            // A multiple of both?
+public class fizzBuzz {    
+  public static void main(String[] args) {  
+    for(int i = 1; i <= 100; i++) {        
+      if (((i % 3) == 0) && ((i % 5) == 0))
         System.out.println("fizzbuzz");    
-      else if ((i % 3) == 0) System.out.println("fizz"); // else a multiple of 5?
-      else if ((i % 5) == 0) System.out.println("buzz"); // else a multiple of 7?
-      else System.out.println(i);                        // else just print it
+      else if ((i % 3) == 0) System.out.println("fizz"); 
+      else if ((i % 5) == 0) System.out.println("buzz"); 
+      else System.out.println(i);                       
       System.out.print(" "); 
     }
     System.out.println();
